4e62a9ec189628d1603715e26895649c50cde520
Pulkit Katdare <radiantpulkit@gmail.com>
2016-03-01 03:23:50 +0530

    detected linesegments in the image

--- vanishing_points.py
+++ vanishing_points.py
@@ -11,10 +11,28 @@ edges = cv2.Canny(img,100,200);
 #img = np.asarray(img)
 #edges = np.asarray(edges)
 print np.shape(img)
-cv2.imwrite('edges.jpg',edges)
-im1 = Image.open('groundtruth/Images/1-deer-valley-living-room6.jpg',);
-im1 = np.asarray(im1);   
+
 #print im1.dtype; Check points in the code 
-im1 = im1.astype(float);
-#print im1.dtype; #check points in the code 
-	
+#print im1.dtype; #check points in the code
+###############################
+img = cv2.imread('groundtruth/Images/1-deer-valley-living-room6.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+minLineLength = 100
+maxLineGap = 5
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+for x1,y1,x2,y2 in lines[0]:
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+cv2.imwrite('houghlines5.jpg',img)
+#print np.shape(lines[0])Check point for lines data type 
+lines = lines.astype(float);
+#####################The part after this is only for testing ##########333
+line = lines[0];
+m1 = (line[0,3]-line[0,1])/(line[0,2]-line[0,0]);
+m2 = (line[1,3]-line[1,1])/(line[1,2]-line[1,0]);
+#print m1,m2 Another testing point 
+x_intersection =( line[1,1] - line[0,1])/(m1-m2) + x1;
+y_intersection =line[1,1] + m2*(x_intersection - line[1,0]);
+print x_intersection , y_intersection
+
